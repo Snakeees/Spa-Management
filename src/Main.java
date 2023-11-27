e903604962f8219035638df3456033cb8a117a2a
Siddhartha Palreddy <siddhartha@palreddy.in>
2023-11-27 17:13:57 +0530

    passing object to login screen for message display

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        //WelcomeScreen welcomeScreen = new WelcomeScreen();
-        //welcomeScreen.setVisible(true);
+//        WelcomeScreen welcomeScreen = new WelcomeScreen();
+//        welcomeScreen.setVisible(true);
 
-        LoginScreen loginScreen = new LoginScreen();
+        LoginScreen loginScreen = new LoginScreen(null);
         loginScreen.setVisible(true);
 
-        //AppointmentScreen appointmentScreen = new AppointmentScreen();
-        //appointmentScreen.setVisible(true);
+//        AppointmentScreen appointmentScreen = new AppointmentScreen();
+//        appointmentScreen.setVisible(true);
 
 
-        //AdminDashboardScreen adminDashboardScreen = new AdminDashboardScreen();
-        //adminDashboardScreen.setVisible(true);
+//        NonAdminDashboardScreen adminDashboardScreen = new NonAdminDashboardScreen(loginScreen.getUserId("admin"));
+//        adminDashboardScreen.setVisible(true);
+//        AdminDashboardScreen adminDashboardScreen = new AdminDashboardScreen(loginScreen.getUserId("admin"));
+//        adminDashboardScreen.setVisible(true);
     }
 }
