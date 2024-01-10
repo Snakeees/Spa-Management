f304ef59e478448bc4988b20484fc5308acb3d5d
Siddhartha Palreddy <Siddhartha@palreddy.in>
2024-01-10 12:11:51 +0530

    indent

--- src/com/spa/screens/ServicePanel.java
+++ src/com/spa/screens/ServicePanel.java
@@ -14,7 +14,7 @@ import java.util.Date;
 
 public class ServicePanel extends JPanel {
 
-        Service service;
+    Service service;
     private JLabel activeLable;
     private JLabel addServiceLabel;
     private JLabel costLabel;
