ce4bbdfa0f4732b4ea2219b9dd8579b57403c3db
Siddhartha Palreddy <Siddhartha@palreddy.in>
2024-02-01 19:09:51 +0530

    image scaling fixed

--- src/com/spa/dto/NoScalingIcon.java
+++ src/com/spa/dto/NoScalingIcon.java
@@ -79,82 +79,7 @@ public class NoScalingIcon implements Icon
         return icon.getIconHeight();
     }
 
-    public void paintIcon(Component c, Graphics g, int x, int y)
-    {
-        /*Graphics2D g2d = (Graphics2D)g.create();
-
-        AffineTransform at = g2d.getTransform();
-        int scaleX = (int) (x * at.getScaleX());
-        int scaleY = (int) (y * at.getScaleY());
-
-
-        AffineTransform scaled = AffineTransform.getScaleInstance(1.0 / at.getScaleX(), 1.0 / at.getScaleY());
-        at.concatenate( scaled );
-        g2d.setTransform( at );
-
-        g2d.drawImage(getScaled(), scaleX, scaleY, c);
-
-        Image scaledImage = getImage().getScaledInstance((int) (getIconWidth()*1.5), (int) (getIconHeight()*1.5),  java.awt.Image.SCALE_SMOOTH);
-        g2d.drawImage(scaledImage, x, y, c);
-
-        g2d.dispose();*/
-
-        /*Graphics2D g2d = (Graphics2D)g.create();
-
-        AffineTransform at = g2d.getTransform();
-
-        AffineTransform scaled = AffineTransform.getScaleInstance(1.0 / at.getScaleX(), 1.0 / at.getScaleY());
-        at.concatenate( scaled );
-        g2d.setTransform( at );
-
-        MediaTracker tracker = new MediaTracker(c);
-
-        while ((invert && !invertComplete) || (scale && !scaleComplete)) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                return;
-            }
-        }
-
-
-        Image scaledImage = getImage().getScaledInstance(this.IconWidth, this.IconHeight, Image.SCALE_SMOOTH);
-        ImageIcon imageIcon = new ImageIcon(scaledImage);
-
-
-
-        // Using MediaTracker to wait for the image to load
-        tracker.addImage(scaledImage, 0);
-
-        try {
-            tracker.waitForAll();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return;
-        }
-
-        System.out.println("");
-        System.out.println("Height: " + imageIcon.getIconHeight());
-        System.out.println("Width: " + imageIcon.getIconWidth());
-
-
-        System.out.println("x: " + x);
-        System.out.println("y: " + y);
-        //System.out.println("scaleX: " + scaleX);
-        //System.out.println("scaleY: " + scaleY);
-        if (!tracker.isErrorAny()) {
-            this.icon = imageIcon;
-            //this.icon.paintIcon(c, g2d, x, y);
-
-            g2d.drawImage(scaledImage, x, y, c);
-        } else {
-            this.icon.paintIcon(c, g2d, x, y);
-
-            //g2d.drawImage(getImage(), x, y, c);
-        }
-        g2d.dispose();*/
-
+    public void paintIcon(Component c, Graphics g, int x, int y) {
         Graphics2D g2d = (Graphics2D)g.create();
 
         AffineTransform at = g2d.getTransform();
@@ -165,8 +90,7 @@ public class NoScalingIcon implements Icon
         at.concatenate( scaled );
         g2d.setTransform( at );
 
-
-        Image scaledImage = getImage().getScaledInstance((int) (getIconWidth() * 1.5), (int) (getIconHeight() * 1.5), Image.SCALE_SMOOTH);
+        Image scaledImage = getImage().getScaledInstance((int) (getIconWidth() * scaleX), (int) (getIconHeight() * scaleY), Image.SCALE_SMOOTH);
 
         // Using MediaTracker to wait for the image to load
         MediaTracker tracker = new MediaTracker(c);
